4d235b695e732009d0872a8201c3dba0b34a4729
Maxim <Maks3d47@gmail.com>
2023-06-29 13:16:58 +0300

    prepare to deploy on railway

--- src/main/java/ru/maxim/effectivemobiletesttask/EffectiveMobileTestTaskApplication.java
+++ src/main/java/ru/maxim/effectivemobiletesttask/EffectiveMobileTestTaskApplication.java
@@ -1,13 +1,22 @@
 package ru.maxim.effectivemobiletesttask;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
 public class EffectiveMobileTestTaskApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EffectiveMobileTestTaskApplication.class, args);
+    }
+
+    @Profile({"railway"})
+    @Bean
+    CommandLineRunner run(){
+        return args -> {};
     }
 
 }
